296cc1c47d0a26dc4696c2975cf4ff735d488821
Polly L-G <plinkoglegabriel@gmail.com>
2024-10-27 09:40:05 +0000

    adding user input validation

--- src/components/FormPopup.jsx
+++ src/components/FormPopup.jsx
@@ -10,6 +10,7 @@ const FormPopup = ({ onSubmit }) => {
         { name: 'Entertainment', amount: 50 },
         { name: 'Other', amount: 50 },
     ]);
+    const [error, setError] = useState('');
 
     const handleCategoryChange = (index, amount) => {
         const updatedCategories = [...categories];
@@ -19,12 +20,19 @@ const FormPopup = ({ onSubmit }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        onSubmit(income, categories);
+        const totalCategoryAmount = categories.reduce((total, category) => total + category.amount, 0);
+        if (totalCategoryAmount > income) {
+            setError('Total category amounts exceed monthly income.');
+        } else {
+            setError('');
+            onSubmit(income, categories);
+        }
     };
 
     return (
         <div className="popup">
             <h2 className='formtitle'>Enter Your Budget Details</h2>
+            <p className='text'>(Prefilled with averages)</p>
             <form onSubmit={handleSubmit}>
                 <div>
                     <label className='popuptext'>Monthly Income: £ </label>
@@ -46,6 +54,7 @@ const FormPopup = ({ onSubmit }) => {
                         />
                     </div>
                 ))}
+                {error && <p className="errormessage" style={{ color: 'red' }}>{error}</p>}
                 <button id="fly" type="submit">Submit</button>
             </form>
         </div>
